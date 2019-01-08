e698dc4d7ab2e905fe53ede971efa1abfb614eb2
AldairMC <aldairmc2023@gmail.com>
2019-01-08 16:30:19 -0500

    ruby: two-fer 4 passed - 0 failed - finished - 2° iteration rubocop no offenses

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -4,10 +4,6 @@ end
 # two_fer exercism
 class TwoFer
   def self.two_fer(name = 'you')
-    if name == 'you'
-      'One for you, one for me.'
-    else
-      "One for #{name}, one for me."
-    end
+    "One for #{name}, one for me."
   end
 end
