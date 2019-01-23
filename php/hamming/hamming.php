69af997ec794560b0f87a83c5b1e25bbc37ec40a
AldairMC <aldairmc2023@gmail.com>
2019-01-23 12:52:12 -0500

    php: hamming 9 passed - 0 failed - finished - 2° Iteration

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -5,15 +5,13 @@ function distance($a, $b)
   $cont = 0;
   $arr1 = str_split($a);
   $arr2 = str_split($b);
-    if (count($arr1) != count($arr2)) {
-        throw new InvalidArgumentException('DNA strands must be of equal length.');
-    }else {
-      for ($i=0; $i < count($arr1); $i++) {
-        if ($arr1[$i] != $arr2[$i]) {
+    if (count($arr1) != count($arr2)):
+      throw new InvalidArgumentException('DNA strands must be of equal length.');
+    endif;
+    for ($i=0; $i < count($arr1); $i++):
+        if ($arr1[$i] != $arr2[$i]):
           $cont++;
-        }
-      }
-      return $cont;
-    }
-
+        endif;
+    endfor;
+    return $cont;
 }
