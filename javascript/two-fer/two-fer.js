73cb75fdb769388c798a710e7ae0618fd8a11d77
AldairMC <aldairmc2023@gmail.com>
2019-03-28 22:51:47 -0500

    javascript: two-fer 6 passed - 0 failed - finished - 2°iteration

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,7 +1,4 @@
 export function twoFer(name){
-  if (name === '') {
-    return 'One for you, one for me.';
-  } else {
-    return `One for ${name}, one for me.`;
-  }
+  name = name || 'you';
+  return `One for ${name}, one for me.`;
 }
