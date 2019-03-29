d20bbc7a4860450d42c046813c54caaa1b620275
AldairMC <aldairmc2023@gmail.com>
2019-03-29 16:47:08 -0500

    javescript: resistor-color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,15 @@
+export const colorCode = (str) =>{
+    var code = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+    
+    const COLORS = () => {
+        return code;
+    }
+ 
+    for (let index = 0; index < code.length; index++) {
+        if(code[index] = str){
+            return code[str];
+        }
+    }
+}
+
+ 
