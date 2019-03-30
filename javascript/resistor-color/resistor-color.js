825cde0d818933a99d293bddabb8de0dd3431373
AldairMC <aldairmc2023@gmail.com>
2019-03-30 10:27:26 -0500

    javascript: resistor-color 3 passed - 1 failed

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,15 +1,15 @@
 export const colorCode = (str) =>{
-    var code = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
-    
-    const COLORS = () => {
-        return code;
-    }
+    var code = ["black" ,"brown" ,"red" ,"orange" ,"yellow" ,"green" ,"blue" ,"violet" ,"grey" ,"white"];
  
     for (let index = 0; index < code.length; index++) {
-        if(code[index] = str){
-            return code[str];
+        if(code[index] === str){
+            return index;
         }
     }
+}
+
+export const COLORS = ( ) => {
+    return ["black" ,"brown" ,"red" ,"orange" ,"yellow" ,"green" ,"blue" ,"violet" ,"grey" ,"white"];
 }
 
  
