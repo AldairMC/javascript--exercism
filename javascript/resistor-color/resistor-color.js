557e4ed6035af4f3109db03dc87582ed829804b3
AldairMC <aldairmc2023@gmail.com>
2019-04-09 13:14:22 -0500

    javascript: resistor-color 4 passed - 0 failed - finished - 2° iteration

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,12 +1,6 @@
 export const COLORS =  ["black" ,"brown" ,"red" ,"orange" ,"yellow" ,"green" ,"blue" ,"violet" ,"grey" ,"white"];
 
-export const colorCode = (str) => {
-    for (let index = 0; index < COLORS.length; index++) {
-        if(COLORS[index] === str){
-            return index;
-        }
-    }
-}
+export const colorCode = (str) => COLORS.indexOf(str);
 
 
 
