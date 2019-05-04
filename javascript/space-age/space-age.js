4a45c9bf8707f22f1a47c091bff72d10cadab751
AldairMC <aldairmc2023@gmail.com>
2019-05-04 15:28:29 +0000

    javascript: space-age - 8 passed - 0 failed - finished - 2° iteration

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,34 +1,21 @@
- const convert = (planet, time) => {
-    switch(planet){
-      case 'earth':
-        return time / 31557600
-      break;
-      case 'mercury':
-        return time / (0.2408467*31557600)
-      break;
-      case 'venus':
-        return time / (0.61519726*31557600)
-      break;
-      case 'mars':
-        return time / (1.8808158*31557600)
-      break;
-      case 'jupiter':
-        return time / (11.862615*31557600)
-      break;
-      case 'saturn':
-        return time / (29.447498*31557600)
-      break;
-      case 'uranus':
-        return time / (84.016846*31557600)
-      break;
-      case 'neptune':
-        return time / (164.79132*31557600)
-      break;
+const AGE_PLANETS = {
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'earth': 1,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132}
+ 
+const earth = 31557600;
+
+const age = (planet, time) =>{
+  for(let i in AGE_PLANETS){
+    if(i === planet){
+      return Number((time / (AGE_PLANETS[i] * earth)).toFixed(2))
     }
   }
-
-  const age = (planet, time) => {
-    return Number(convert(planet, time).toFixed(2))
-  }
+}
   
-  export {age}
+export {age}
