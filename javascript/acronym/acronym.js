b086a78658c830924429a4ac1cf60ff23a87f47a
AldairMC <aldairmc2023@gmail.com>
2019-05-10 22:07:14 +0000

    javascript: acronym 9 passed - 0 failed - finished - 1° iteration

---
+++ javascript/acronym/acronym.js
@@ -0,0 +1,7 @@
+export const parse = (str) => {
+  return str.replace(/\s+|[_]|[-]|\W^[']/g, ' ')
+            .split(' ')
+            .map(i => i[0])
+            .join('')
+            .toUpperCase()
+}
