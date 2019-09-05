de57a69b20ecfb8e4f603c731cc99441e4668be5
AldairMC <aldairmc2023@gmail.com>
2019-09-05 12:02:31 -0500

    javascript: grains

---
+++ javascript/grains/grains.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Grains' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Grains {
+  square(num) {
+    const sq = 2**(num-1)
+    return BigInt(sq).toString()
+  }
+
+  total() {
+    
+  }
+}
