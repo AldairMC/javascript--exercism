f3a320ade42dc039ddd43e458f303a3289ef6ff8
AldairMC <aldairmc2023@gmail.com>
2019-10-21 21:18:39 +0000

    javascript: anagram

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,26 @@
+//
+// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Anagram {
+  constructor(word) {
+    this.word = word      
+  }
+
+  matches() {
+      
+  }
+}
+
+// class Anagram{
+//   constructor(number){
+//     this.number = number
+//   }
+
+//   index(array){
+//     let str = this.number.split('')
+//     return str
+//     return array.map(i => i*2)
+//   }
+// }
