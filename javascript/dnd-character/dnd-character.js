905dd60f8b1601f97c41dee0ee5a63952a2f6d7a
AldairMC <aldairmc2023@gmail.com>
2019-12-09 22:41:30 +0000

    :construction: and :bug: javascript: dnd-character 16 passed - 5 failed

---
+++ javascript/dnd-character/dnd-character.js
@@ -0,0 +1,43 @@
+//
+// This is only a SKELETON file for the 'DnD Character' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const abilityModifier = (num) => {
+  const difference = (num - 10)/2
+  return Math.floor(difference)  
+};
+
+export class Character {
+  static rollAbility() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get strength() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get dexterity() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get constitution() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get intelligence() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get wisdom() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get charisma() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get hitpoints() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
