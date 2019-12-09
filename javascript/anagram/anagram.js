49fea61b891f8c712a217c0c1a74caa83f9b62f5
AldairMC <aldairmc2023@gmail.com>
2019-12-09 19:21:47 +0000

    :white_check_mark: and :sparkles: javascript: anagram 12 passed - 0 failed - finished - 1° iteration

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,18 +1,17 @@
-//
 // This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 export class Anagram {
   constructor(word) {
-    this.word = word     
+    this.word = word.toUpperCase()    
   }
 
   matches(array) {
     const NUEVO = []
     let x = array.map( i => 
-      (i.split('').sort().join('').toLowerCase())
+      (i.toUpperCase().split('').sort().join(''))
     )
     x.map((i, j) => {
-      if(i === this.word.split('').sort().join('')){
+      if(i === this.word.split('').sort().join('') && this.word !== array[j].toUpperCase()){
         NUEVO.push(array[j])
       }
     })
