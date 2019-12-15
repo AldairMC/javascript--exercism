6c38ef6f44462e52412aff70664ee5d2b4936b0d
AldairMC <aldairmc2023@gmail.com>
2019-12-15 04:20:43 +0000

    :white_check_mark: javascript: bob 25 passed - 0 failed - finished - 1° iteration

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -4,11 +4,7 @@
 // convenience to get you started writing code faster.
 //
 const auxiliar = (sentence) => {
-  let y = /[a-z]/g
-  return sentence
-    .split('')
-    .map(i => y.test(i))
-    .reduce((a,b) => a || b)
+  return /^[A-Z]+$/.test(sentence.replace(/\W|\d/g, ''))
 }
 
 export const hey = (message) => {
@@ -19,9 +15,13 @@ export const hey = (message) => {
     .join('')
   
   if(Number(message) === 0) return 'Fine. Be that way!'
-  else if(aux[0] === '?' && !auxiliar(aux)) return 'Calm down, I know what I\'m doing!'
-  else if(aux[0] !== '?' && !auxiliar(aux)) return 'Whoa, chill out!'
-  else if(aux[0] === '?' && (auxiliar(aux) || /\W/g.test(aux))) return 'Sure.'
+  else if(aux[0] === '?'){
+    if(!auxiliar(aux)){
+      return 'Sure.'
+    }else{
+      return 'Calm down, I know what I\'m doing!' 
+    } 
+  }else if(aux[0] !== '?' && auxiliar(aux)) return 'Whoa, chill out!'
   else return 'Whatever.'
 };
 
