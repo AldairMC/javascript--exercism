04617101dfe22c30e1ef3c9313c2afc1f8c7cdb7
AldairMC <aldairmc2023@gmail.com>
2019-12-17 15:11:31 +0000

    :art: javascript: bob 25 passed - 0 failed - finished - 1° iteration

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,27 +1,18 @@
 /* eslint-disable no-unused-vars */
 //
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// Bob' exercise
+
 const auxiliar = (sentence) => {
   return /^[A-Z]+$/.test(sentence.replace(/\W|\d/g, ''))
 }
 
 export const hey = (message) => {
-  let aux = message
-    .replace(/\s/g, '')
+  let aux = message.trim()
   
   if(Number(message) === 0) return 'Fine. Be that way!'
   else if(aux[aux.length-1] === '?'){
-    if(!auxiliar(aux)){
-      return 'Sure.'
-    }else{
-      return 'Calm down, I know what I\'m doing!' 
-    }
+    if(!auxiliar(aux)) return 'Sure.'
+     return 'Calm down, I know what I\'m doing!' 
   }else if(aux[aux.length-1] !== '?' && auxiliar(aux)) return 'Whoa, chill out!'
-  else return 'Whatever.'
+  return 'Whatever.'
 };
-
-
-
-
