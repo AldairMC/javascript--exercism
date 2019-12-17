086ee37f97d05be5113990db27fbeb6b67c91853
AldairMC <aldairmc2023@gmail.com>
2019-12-17 18:44:05 +0000

    :white_check_mark: javascript: bob 25 passed - 0 failed - finished - 2° iteration

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -2,10 +2,11 @@
 export const hey = (message) => {
   let aux = message.trim()
   let regExp = /^[A-Z]+$/.test(message.replace(/\W|\d/g, ''))
+//   let regExp = aux.toUpperCase() === aux
   
   if(aux === '') return 'Fine. Be that way!'
-  else if(aux.slice(-1) === '?'){
-    return !regExp? 'Sure.' : 'Calm down, I know what I\'m doing!' 
-  }else if(aux.slice(-1) !== '?' && regExp) return 'Whoa, chill out!'
+  if(aux.slice(-1) === '?')
+return !regExp? 'Sure.' : 'Calm down, I know what I\'m doing!' 
+  if(aux.slice(-1) !== '?' && regExp) return 'Whoa, chill out!'
   return 'Whatever.'
 }
