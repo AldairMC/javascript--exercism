6152d812c786d41ef6d606761d2351aad2c59f2e
AldairMC <aldairmc2023@gmail.com>
2019-12-17 16:19:34 +0000

    :art: javascript: bob 25 passed - 0 failed - finished - 1° iteration

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,11 @@
-/* eslint-disable no-unused-vars */
-//
-// Bob' exercise
+// Bob exercise
 export const hey = (message) => {
   let aux = message.trim()
   let regExp = /^[A-Z]+$/.test(message.replace(/\W|\d/g, ''))
   
-  if(Number(message) === 0) return 'Fine. Be that way!'
+  if(aux === '') return 'Fine. Be that way!'
   else if(aux.slice(-1) === '?'){
-    if(!regExp) return 'Sure.'
-     return 'Calm down, I know what I\'m doing!' 
+    return !regExp? 'Sure.' : 'Calm down, I know what I\'m doing!' 
   }else if(aux.slice(-1) !== '?' && regExp) return 'Whoa, chill out!'
   return 'Whatever.'
-};
+}
