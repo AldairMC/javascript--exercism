c12ce701f702e8faac1eaf20341e4bdeb60bb13f
AldairMC <aldairmc2023@gmail.com>
2019-12-19 18:45:17 +0000

    :white_check_mark: and :art: javascript: scrabble-score 12 passed - 0 failed - finished - 2° iteration

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,20 +1,19 @@
 //Scrabble-score exercise
-const LETTER = {
-  'AEIOULNRSTaeioulnrst': 1,
-  'DGdg': 2,
-  'BCMPbcmp': 3,
-  'FHVWYfhvwy': 4,
-  'Kk': 5,
-  'JXjx': 8,
-  'QZqz': 10
+const LETTERS = {
+  'aeioulnrst': 1,
+  'dg': 2,
+  'bcmp': 3,
+  'fhvwy': 4,
+  'k': 5,
+  'jx': 8,
+  'qz': 10
 }
 
 export const score = (word) => {
-  if(word === '') return 0
   return word
     .split('')
-    .map(i => Object.keys(LETTER))
-    .map((i, k) => i.filter((i) => i.includes(word[k])))
-    .map(i => LETTER[i[0]])
-    .reduce((a,b) => a+b)
+    .map((i, j) => Object.keys(LETTERS)
+      .filter(i => i.includes(word[j].toLowerCase())))
+    .map(i => LETTERS[i[0]])
+    .reduce((a,b) => a+b, 0)
 };
