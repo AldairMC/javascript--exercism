08d1bfd6e55b84aa8fb315a7f9f054187a1b2abb
AldairMC <aldairmc2023@gmail.com>
2019-12-26 19:04:26 +0000

    :white_check_mark: javascript: pythagorean-triplet 7 passed - 0 failed - finished - 1° iteration

--- javascript/pythagorean-triplet/pythagorean-triplet.js
+++ javascript/pythagorean-triplet/pythagorean-triplet.js
@@ -8,17 +8,21 @@ export class Triplet {
     this.b = b
     this.c = c
   }
+  
+  hasRule(){
+    return (this.a < this.b && this.b < this.c)
+  }
 
   sum() {
-    return (this.a + this.b + this.c)
+    if(this.hasRule()) return [this.a, this.b, this.c].reduce((a,b) => a+b)
   }
 
   product() {
-    return [this.a, this.b, this.c].reduce((a,b) => a*b)
+    if(this.hasRule()) return [this.a, this.b, this.c].reduce((a,b) => a*b)
   }
 
   isPythagorean() {
-    return (Math.pow(this.a, 2) + Math.pow(this.b, 2) === Math.pow(this.c, 2))
+    if(this.hasRule()) return (Math.pow(this.a, 2) + Math.pow(this.b, 2) === Math.pow(this.c, 2))
   }
 
   static where(obj) {
@@ -27,12 +31,9 @@ export class Triplet {
       for(let j = obj.minFactor || 1; j <= obj.maxFactor; j++){
         for(let k = obj.minFactor || 1; k <= obj.maxFactor; k++){
           let triplet = new Triplet(i, j, k);
-          if((i < j && j < k) && triplet.isPythagorean()){
-            if(!obj.sum){
-              aux2.push(triplet)
-            }else{
-              if(triplet.sum() == 180 ) aux2.push(triplet)
-            }
+          if((triplet.hasRule()) && triplet.isPythagorean()){
+            if(!obj.sum) aux2.push(triplet)
+            if(triplet.sum() == 180 ) aux2.push(triplet)
           }
         }
       } 
