bb1b8c7f151441b620fdf0968f4aacc5e92d168f
AldairMC <aldairmc2023@gmail.com>
2019-12-26 02:57:47 +0000

    :construction: and :bug: javascript: pythagorean-triplet 5 passed - 1 failed

--- javascript/pythagorean-triplet/pythagorean-triplet.js
+++ javascript/pythagorean-triplet/pythagorean-triplet.js
@@ -2,7 +2,6 @@
 // This is only a SKELETON file for the 'Pythagorean Triplet' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
 export class Triplet {
   constructor(a, b, c) {
     this.a = a
@@ -15,14 +14,25 @@ export class Triplet {
   }
 
   product() {
-    return (this.a * this.b * this.c)
+    return [this.a, this.b, this.c].reduce((a,b) => a*b)
   }
 
   isPythagorean() {
     return (Math.pow(this.a, 2) + Math.pow(this.b, 2) === Math.pow(this.c, 2))
   }
 
-  static where() {
-    throw new Error("Remove this statement and implement this function");
+  static where(obj) {
+    let aux2 = []
+    for(let i = obj.minFactor || 1; i <= obj.maxFactor; i++){
+      for(let j = obj.minFactor || 1; j <= obj.maxFactor; j++){
+        for(let k = obj.minFactor || 1; k <= obj.maxFactor; k++){
+          let triplet = new Triplet(i, j, k);
+          if((i < j && j < k)){
+            if(triplet.sum() == 180 && triplet.isPythagorean()) aux2.push(triplet);
+          }
+        }
+      } 
+    }
+    return aux2;
   }
 }
