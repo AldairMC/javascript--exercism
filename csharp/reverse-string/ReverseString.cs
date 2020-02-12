fb9d8840efc558ca4ce3fc151cd0790812b21904
AldairMC <aldairmc2023@gmail.com>
2020-02-12 14:26:04 -0500

    :white_check_mark: C#: reverse-string 6 passed - 0 failed - finished - 1° iteration

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ReverseString {
+    public static string Reverse(string input) {
+        string aux = "";
+        for(int i = input.Length-1; i >= 0; i--){
+            aux += input[i];
+        }
+        return aux;
+    }
+}
