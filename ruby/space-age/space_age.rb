0af540ed8545a74bc1e89d005128183df174c41f
AldairMC <aldairmc2023@gmail.com>
2021-02-16 16:53:23 -0500

    :white_check_mark: ruby: space-age 8 passed - 0 failed - finished - 1° iteration | 0 offenses

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -5,8 +5,15 @@ class SpaceAge
   EARTH = 31_557_600
 
   TIME_FOR_PLANET = {
-    'mercury' => 0.2408467
-  }
+    'mercury' => 0.2408467,
+    'venus' => 0.61519726,
+    'earth' => 1.0,
+    'mars' => 1.8808158,
+    'jupiter' => 11.862615,
+    'saturn' => 29.447498,
+    'uranus' => 84.016846,
+    'neptune' => 164.79132
+  }.freeze
 
   attr_reader :time
 
@@ -14,11 +21,39 @@ class SpaceAge
     @time = time
   end
 
-  def on_mercury
-    self.calculated('mercury')
+  def calculated(planet)
+    (time / (TIME_FOR_PLANET[planet] * EARTH)).round(2)
   end
 
-  def self.calculated(planet)
-    (time / (TIME_FOR_PLANET[planet] * EARTH)).round(2)
+  def on_mercury
+    calculated('mercury')
+  end
+
+  def on_venus
+    calculated('venus')
+  end
+
+  def on_earth
+    calculated('earth')
+  end
+
+  def on_mars
+    calculated('mars')
+  end
+
+  def on_jupiter
+    calculated('jupiter')
+  end
+
+  def on_saturn
+    calculated('saturn')
+  end
+
+  def on_uranus
+    calculated('uranus')
+  end
+
+  def on_neptune
+    calculated('neptune')
   end
 end
