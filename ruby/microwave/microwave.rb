8e27d000d49496af5433ccdae770f97866e0bd3b
AldairMC <aldairmc2023@gmail.com>
2021-02-22 16:34:38 -0500

    :construction: ruby: microwave

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
 # Microwave exercism
-class  Microwave
-  attr_reader :time
+class Microwave
+  attr_reader :buttons
 
-  def initialize(time)
-    @time = time
+  def initialize(buttons)
+    @buttons = buttons
+  end
+
+  def minutes
+    num = buttons.to_s[0]
+    "0#{num}:00"
+  end
+
+  def seconds
+    minutes = buttons / 60
+    seconds = buttons - (minutes.floor * 60)
+    "0#{minutes.floor}:#{seconds}"
   end
 
   def timer
-    0
+    if buttons >= 2 && (buttons % 100).zero?
+      get_minutes
+    else
+      get_seconds
+    end
   end
 end
