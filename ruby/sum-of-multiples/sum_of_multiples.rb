f044c0130a286a06c45d96333a808f3b498e523c
AldairMC <aldairmc2023@gmail.com>
2021-05-22 23:41:21 -0500

    :art: ruby: sum-of-multiples 15 passed - 0 failed - finished - 3° iteration | 0 offense | Rename variables

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -9,8 +9,10 @@ class SumOfMultiples
   end
 
   def to(limit)
-    return 0 if args.empty? || args[0].zero?
+    return 0 if args.empty? || args.first.zero?
 
-    (args.min...limit).select { |ele| args.any? { |itr| (ele % itr).zero? } }.sum
+    (args.min...limit).select do |number|
+      args.any? { |i| (number % i).zero? }
+    end.sum
   end
 end
