8f81424273336ab43b68616fcb4be5f6a957b097
AldairMC <aldairmc2023@gmail.com>
2021-05-30 23:05:46 -0500

    :sparkles: ruby: phone-number 18 passed - 0 failed - finished - 1° iteration | 7 offenses

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,10 +2,10 @@
 
 # Exercism phone number
 class PhoneNumber
-
-  NUMBER_PHOME_VALID = '2234567890'
-
   def self.clean(number)
-    number.gsub(/\D/, '')
+    number_phone = number.gsub(/\D/, '')
+    return number_phone if number_phone.size == 10 && number_phone[3].to_i.between?(2, 9) && number_phone[0].to_i.between?(2, 9)
+    return nil if number_phone.size < 10 && number_phone.size > 11
+    return number_phone[1..-1] if number_phone.size == 11 && number_phone[1..-1][0].to_i.between?(2, 9) && number_phone[1..-1][3].to_i.between?(2, 9) && number_phone[0].to_i == 1
   end
 end
