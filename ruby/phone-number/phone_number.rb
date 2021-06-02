8147b1bdc07aa444f72bda9f33cbfda58ba35a87
AldairMC <aldairmc2023@gmail.com>
2021-06-02 17:03:12 -0500

    :art::white_check_mark: ruby: phone-number 18 passed - 0 failed - finished - 2° iteration | 0 offense | Refactor method clean

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -3,18 +3,30 @@
 # Exercism phone number
 class PhoneNumber
   def self.clean(number)
-    number_phone = number.gsub(/\D/, '')
-    return number_phone if number_phone_equal_ten_digits(number_phone)
-    return nil if number_phone.size < 10 && number_phone.size > 11
-    return number_phone[1..-1] if number_phone.size == 11 && number_phone[1..-1][0].to_i.between?(2, 9) && number_phone[1..-1][3].to_i.between?(2, 9) && number_phone[0].to_i == 1
+    phone_number = number.gsub(/\D/, '')
+    return phone_number if phone_number_correct(phone_number)
+
+    phone_number[1..-1] if valid?(phone_number) || invalid?(phone_number)
   end
 
-  private
+  class << self
+    def phone_number_correct(number)
+      phone_number = number.size == 10
+      area_code = number[0].to_i.between?(2, 9)
+      exchange_code = number[3].to_i.between?(2, 9)
+      phone_number && area_code && exchange_code
+    end
 
-  def self.number_phone_equal_ten_digits(number)
-    number_phone_length = number.size == 10
-    verify_second_digit = number[3].to_i.between?(2, 9)
-    verify_first_digit = number[0].to_i.between?(2, 9)
-    number_phone_length && verify_first_digit && verify_second_digit
+    def invalid?(number)
+      number.size < 10 && number.size > 11
+    end
+
+    def valid?(number)
+      phone_number = number.size == 11
+      area_code = number[1..-1][0].to_i.between?(2, 9)
+      exchange_code = number[1..-1][3].to_i.between?(2, 9)
+      nanp = number[0].to_i == 1
+      phone_number && area_code && exchange_code && nanp
+    end
   end
 end
