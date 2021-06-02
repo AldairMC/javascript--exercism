42b9069d008ac31b041626e7879e3fb5ff834180
AldairMC <aldairmc2023@gmail.com>
2021-06-01 22:50:04 -0500

    :art: ruby: phone-number Refactor method clean

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -4,8 +4,17 @@
 class PhoneNumber
   def self.clean(number)
     number_phone = number.gsub(/\D/, '')
-    return number_phone if number_phone.size == 10 && number_phone[3].to_i.between?(2, 9) && number_phone[0].to_i.between?(2, 9)
+    return number_phone if number_phone_equal_ten_digits(number_phone)
     return nil if number_phone.size < 10 && number_phone.size > 11
     return number_phone[1..-1] if number_phone.size == 11 && number_phone[1..-1][0].to_i.between?(2, 9) && number_phone[1..-1][3].to_i.between?(2, 9) && number_phone[0].to_i == 1
+  end
+
+  private
+
+  def self.number_phone_equal_ten_digits(number)
+    number_phone_length = number.size == 10
+    verify_second_digit = number[3].to_i.between?(2, 9)
+    verify_first_digit = number[0].to_i.between?(2, 9)
+    number_phone_length && verify_first_digit && verify_second_digit
   end
 end
