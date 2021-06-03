572265159ecf9bd4391efbd456380523b38e3e16
AldairMC <aldairmc2023@gmail.com>
2021-06-02 22:33:05 -0500

    :art::white_check_mark: ruby: phone-number 18 passed - 0 failed - finished - 3° iteration | 0 offense | Refactor method clean

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,31 +2,12 @@
 
 # Exercism phone number
 class PhoneNumber
+  REGEXP_NANP = /^1?[2-9][0-9]{2}[2-9][0-9]{6}$/.freeze
+
   def self.clean(number)
     phone_number = number.gsub(/\D/, '')
-    return phone_number if phone_number_correct(phone_number)
+    return phone_number[1..-1] if phone_number.size == 11 && phone_number.match(REGEXP_NANP)
 
-    phone_number[1..-1] if valid?(phone_number) || invalid?(phone_number)
-  end
-
-  class << self
-    def phone_number_correct(number)
-      phone_number = number.size == 10
-      area_code = number[0].to_i.between?(2, 9)
-      exchange_code = number[3].to_i.between?(2, 9)
-      phone_number && area_code && exchange_code
-    end
-
-    def invalid?(number)
-      number.size < 10 && number.size > 11
-    end
-
-    def valid?(number)
-      phone_number = number.size == 11
-      area_code = number[1..-1][0].to_i.between?(2, 9)
-      exchange_code = number[1..-1][3].to_i.between?(2, 9)
-      nanp = number[0].to_i == 1
-      phone_number && area_code && exchange_code && nanp
-    end
+    phone_number if phone_number =~ REGEXP_NANP
   end
 end
