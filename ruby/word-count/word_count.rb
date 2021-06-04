052e0d680c03118dd291975f3c5a357c60831aaa
AldairMC <aldairmc2023@gmail.com>
2021-06-03 21:08:19 -0500

    :sparkles::white_check_mark: ruby: word-count 11 passed - 0 failed - finished - 1° iteration | 0 offense

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,14 +1,16 @@
-# froze_string_literal: true
+# frozen_string_literal: true
 
 # Exercism Word Count
 class Phrase
   attr_reader :phrase
 
   def initialize(phrase)
-    @phrase = phrase
+    @phrase = phrase.downcase
   end
 
   def word_count
-    phrase.scan(/\b[\w']+\b/i).each_with_object(Hash.new(0)){|a,(k,v)| k[a]+=1}
+    phrase.scan(/\b[\w']+\b/i).each_with_object(Hash.new(0)) do |a, k|
+      k[a] += 1
+    end
   end
 end
